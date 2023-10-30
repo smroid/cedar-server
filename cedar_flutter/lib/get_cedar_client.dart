ecfbc369bec006f755cd314d136298a0eb047fa5
smroid <smr@dt3.org>
2023-10-29 18:33:01 -0700

    minor

--- cedar_flutter/lib/get_cedar_client.dart
+++ cedar_flutter/lib/get_cedar_client.dart
@@ -4,7 +4,7 @@ import 'package:grpc/grpc.dart';
 // For non-web deployments.
 CedarClient getClient() {
   return CedarClient(ClientChannel(
-    '192.168.1.133',
+    '192.168.4.1',
     port: 8080,
     options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
   ));
