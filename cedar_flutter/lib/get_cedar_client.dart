15fd626275ce23c308864a38568a6d8e2bd48391
smroid <smr@dt3.org>
2023-12-29 14:58:34 -0800

    add timeout option to RPC client

--- cedar_flutter/lib/get_cedar_client.dart
+++ cedar_flutter/lib/get_cedar_client.dart
@@ -3,10 +3,13 @@ import 'package:grpc/grpc.dart';
 
 // For non-web deployments.
 CedarClient getClient() {
-  return CedarClient(ClientChannel(
+  return CedarClient(
+    ClientChannel(
 //    '192.168.4.1',
-    '192.168.1.133',
-    port: 8080,
-    options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
-  ));
+      '192.168.1.133',
+      port: 8080,
+      options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+    ),
+    options: CallOptions(timeout: const Duration(seconds: 1)),
+  );
 }
