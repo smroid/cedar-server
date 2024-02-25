c3279e5b612c3e221cae94787d09dfbbc04d6c44
smroid <smr@dt3.org>
2024-02-24 17:52:19 -0800

    use scale_image_mut() instead stretching the peak_image

--- src/scale_image.rs
+++ src/scale_image.rs
@@ -1,10 +1,6 @@
 use image::GrayImage;
 
-// Copy the image, mapping 0..peak_pixel_value to 0..255 by applying a
-// gamma and scale factor.
-pub fn scale_image(image: &GrayImage, peak_pixel_value: u8, gamma: f32)
-                   -> GrayImage {
-    // Compute the lookup table.
+fn compute_lut(peak_pixel_value: u8, gamma: f32) -> [u8; 256] {
     let mut lut: [u8; 256] = [0; 256];
     let scale = 256.0 / (peak_pixel_value as f32).powf(gamma);
     for n in 0..256 {
@@ -16,10 +12,27 @@ pub fn scale_image(image: &GrayImage, peak_pixel_value: u8, gamma: f32)
         };
         lut[n] = clamped;
     }
+    lut
+}
+
+// Copy the image, mapping 0..peak_pixel_value to 0..255 by applying a
+// gamma and scale factor.
+pub fn scale_image(image: &GrayImage, peak_pixel_value: u8, gamma: f32)
+                   -> GrayImage {
+    let lut = compute_lut(peak_pixel_value, gamma);
 
     // Apply the lut
     let out_vec: Vec<u8> = image.as_raw().iter().map(|x| lut[*x as usize]).collect();
 
     let (width, height) = image.dimensions();
     GrayImage::from_raw(width, height, out_vec).unwrap()
+}
+
+// In-place variant of scale_image().
+pub fn scale_image_mut(image: &mut GrayImage, peak_pixel_value: u8, gamma: f32) {
+    let lut = compute_lut(peak_pixel_value, gamma);
+
+    for pixel in image.pixels_mut() {
+        pixel[0] = lut[pixel[0] as usize];
+    }
 }
