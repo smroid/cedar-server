21bb93647276a3fd5ee311aba94fc606ceaff861
smroid <smr@dt3.org>
2024-03-09 09:17:36 -0800

    add equatorial_from_alt_az()

--- src/astro_util.rs
+++ src/astro_util.rs
@@ -1,8 +1,8 @@
-use astro::angle::{limit_to_two_PI};
-use astro::coords::{alt_frm_eq, az_frm_eq};
+use astro::angle::{anglr_sepr, limit_to_two_PI};
+use astro::coords::{alt_frm_eq, az_frm_eq, hr_angl_frm_hz};
 use astro::time::{CalType, Date, julian_day, mn_sidr};
 
-use chrono::{Datelike, DateTime, Timelike};
+use chrono::{Datelike, DateTime, Timelike, Utc};
 use std::f64::consts::{PI};
 use std::time::{SystemTime};
 
@@ -10,8 +10,7 @@ use std::time::{SystemTime};
 /// (in radians).
 pub fn angular_separation(p0_ra: f64, p0_dec: f64,
                           p1_ra: f64, p1_dec: f64) -> f64 {
-    (p0_dec.sin() * p1_dec.sin() +
-     p0_dec.cos() * p1_dec.cos() * (p0_ra - p1_ra).cos()).acos()
+    anglr_sepr(p0_ra, p0_dec, p1_ra, p1_dec)
 }
 
 /// Returns the position angle of p1 relative to p0. Range is -pi..pi,
@@ -32,26 +31,45 @@ pub fn position_angle(p0_ra: f64, p0_dec: f64,
     x.atan2(y)
 }
 
-/// Returns (alt, az)
-/// alt: elevation in radians
-/// az: radians, clockwise from north
+/// Returns (alt, az) in radians. Returned azimuth is clockwise from north.
+/// ra: right ascension in radians.
+/// dec: declination in radians.
+/// lat: observer latitude in radians.
+/// long: observer longitude in radians.
 pub fn alt_az_from_equatorial(ra: f64, dec: f64, lat: f64, long: f64,
                               time: SystemTime) -> (f64, f64) {
-    let mean_sidereal_time = mean_sidereal_time_from_unix_seconds(
-        time.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs_f64());
+    let gmst = greenwich_mean_sidereal_time_from_system_time(time);
 
     // Note that astro::coords::hr_angl_frm_observer_long() has a bug. Fortunately
     // the correct relation is trivial.
-    let hour_angle = mean_sidereal_time + long - ra;
+    let hour_angle = gmst + long - ra;
 
     let meeus_az = az_frm_eq(hour_angle, dec, lat);
-    let civil_az = limit_to_two_PI(meeus_az + PI);
+    let az = limit_to_two_PI(meeus_az + PI);
 
-    (alt_frm_eq(hour_angle, dec, lat), civil_az)
+    (alt_frm_eq(hour_angle, dec, lat), az)
 }
 
-fn mean_sidereal_time_from_unix_seconds(unix_seconds: f64) -> f64 {
-    let dt_utc = DateTime::from_timestamp(unix_seconds as i64, 0).unwrap();
+/// Returns (ra, dec) in radians.
+/// alt: elevation in radians
+/// az: radians, clockwise from north
+/// lat: observer latitude in radians.
+/// long: observer longitude in radians.
+pub fn equatorial_from_alt_az(alt: f64, az: f64, lat: f64, long: f64,
+                              time: SystemTime) -> (f64, f64) {
+    let meeus_az = limit_to_two_PI(az - PI);
+    let gmst = greenwich_mean_sidereal_time_from_system_time(time);
+
+    // astro::coords::dec_frm_hz() is incorrect.
+    let dec = (lat.sin() * alt.sin() - lat.cos() * alt.cos() * meeus_az.cos()).asin();
+    let hour_angle = hr_angl_frm_hz(meeus_az, alt, lat);
+    let ra = gmst + long - hour_angle;
+
+    (ra, dec)
+}
+
+fn greenwich_mean_sidereal_time_from_system_time(time: SystemTime) -> f64 {
+    let dt_utc = DateTime::<Utc>::from(time);
     let date = Date{year: dt_utc.date_naive().year() as i16,
                     month: dt_utc.date_naive().month() as u8,
                     decimal_day: dt_utc.date_naive().day() as f64,
@@ -60,9 +78,8 @@ fn mean_sidereal_time_from_unix_seconds(unix_seconds: f64) -> f64 {
 
     let utc_hours = dt_utc.time().num_seconds_from_midnight() as f64 / 3600.0;
     let gmst_hours = mn_sidr(jd).to_degrees() / 15.0 + utc_hours * 1.00273790935;
-    let gmst_radians = limit_to_two_PI((gmst_hours * 15.0).to_radians());
 
-    gmst_radians
+    limit_to_two_PI((gmst_hours * 15.0).to_radians())
 }
 
 #[cfg(test)]
@@ -102,19 +119,19 @@ mod tests {
     }
 
     #[test]
-    fn test_alt_az_from_equatorial() {
+    fn test_alt_az_equatorial_conversion() {
         let mizar_ra = deg_frm_hms(13, 23, 55.5).to_radians();
         let mizar_dec = deg_frm_dms(54, 55, 30.7).to_radians();
 
         let dt = FixedOffset::west_opt(8 * 3600).unwrap().with_ymd_and_hms(
             2024, 3, 7, 23, 56, 0).unwrap();
-        let unix_time = SystemTime::UNIX_EPOCH.checked_add(Duration::from_secs_f64(
+        let time = SystemTime::UNIX_EPOCH.checked_add(Duration::from_secs_f64(
             dt.timestamp_millis() as f64 / 1000.0)).unwrap();
 
         let lat = 37_f64.to_radians();
         let long = -122_f64.to_radians();
 
-        let (alt, az) = alt_az_from_equatorial(mizar_ra, mizar_dec, lat, long, unix_time);
+        let (alt, az) = alt_az_from_equatorial(mizar_ra, mizar_dec, lat, long, time);
 
         // Expected values obtained from SkySafari.
         assert_abs_diff_eq!(alt,
@@ -122,6 +139,13 @@ mod tests {
                             epsilon = 0.02);
         assert_abs_diff_eq!(az,
                             deg_frm_dms(43, 20, 36.7).to_radians(),
+                            epsilon = 0.02);
+
+        // Now go the other way.
+        let (ra, dec) = equatorial_from_alt_az(alt, az, lat, long, time);
+        assert_abs_diff_eq!(ra, mizar_ra,
+                            epsilon = 0.02);
+        assert_abs_diff_eq!(dec, mizar_dec,
                             epsilon = 0.02);
     }
 
