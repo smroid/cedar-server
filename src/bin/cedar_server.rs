42d8da2d37c734d5ff09d370372561b7fc647d6e
smroid <smr@dt3.org>
2024-05-05 19:17:40 -0700

    improve black level in displayed images and insets

--- src/bin/cedar_server.rs
+++ src/bin/cedar_server.rs
@@ -773,7 +773,10 @@ impl MyCedar {
         let mut bmp_buf = Vec::<u8>::new();
         let (width, height) = disp_image.dimensions();
         bmp_buf.reserve((width * height) as usize);
-        let scaled_image = scale_image(disp_image, Some(peak_value), /*gamma=*/0.7);
+        let scaled_image = scale_image(disp_image,
+                                       detect_result.display_black_level,
+                                       peak_value,
+                                       /*gamma=*/0.7);
         // Save most recent display image.
         state.lock().await.scaled_image = Some(Arc::new(scaled_image.clone()));
         scaled_image.write_to(&mut Cursor::new(&mut bmp_buf),
--- src/scale_image.rs
+++ src/scale_image.rs
@@ -1,39 +1,33 @@
 use image::GrayImage;
 
-fn get_peak_pixel(image: &GrayImage) -> u8 {
-    let mut peak_value = 0_u8;
-    for pixel in image.pixels() {
-        if pixel[0] > peak_value {
-            peak_value = pixel[0];
-        }
-    }
-    peak_value
-}
-
-fn compute_lut(peak_pixel_value: Option<u8>, gamma: f32) -> [u8; 256] {
+fn compute_lut(min_pixel_value: u8,
+               peak_pixel_value: u8,
+               gamma: f32) -> [u8; 256] {
     let mut lut: [u8; 256] = [0; 256];
-    let scale = 256.0 / (peak_pixel_value.unwrap() as f32).powf(gamma);
-    for n in 0..256 {
-        let mapped = scale * (n as f32).powf(gamma);
-        let clamped = if mapped >= 255.0 {
-            255_u8
-        } else {
-            mapped as u8
-        };
-        lut[n] = clamped;
+    let scale = 256.0 / ((peak_pixel_value - min_pixel_value) as f32).powf(gamma);
+    for n in 0..=255 {
+        if n < min_pixel_value {
+            lut[n as usize] = 0;
+            continue;
+        }
+        if n >= peak_pixel_value {
+            lut[n as usize] = 255;
+            continue;
+        }
+        lut[n as usize] = (scale * ((n - min_pixel_value) as f32).powf(gamma)) as u8;
     }
     lut
 }
 
-// Copy the image, mapping 0..peak_pixel_value to 0..255 by applying a
-// gamma and scale factor. If `peak_pixel_value` is omitted it is computed
-// from the image.
-pub fn scale_image(image: &GrayImage, mut peak_pixel_value: Option<u8>, gamma: f32)
-                   -> GrayImage {
-    if peak_pixel_value.is_none() {
-        peak_pixel_value = Some(get_peak_pixel(image));
+// Copy the image, mapping min_pixel_value..peak_pixel_value to 0..255 by
+// applying a gamma and scale factor.
+pub fn scale_image(
+    image: &GrayImage, mut min_pixel_value: u8, peak_pixel_value: u8, gamma: f32)
+    -> GrayImage {
+    if min_pixel_value > peak_pixel_value / 2 {
+        min_pixel_value = peak_pixel_value / 2;
     }
-    let lut = compute_lut(peak_pixel_value, gamma);
+    let lut = compute_lut(min_pixel_value, peak_pixel_value, gamma);
 
     // Apply the lut
     let out_vec: Vec<u8> = image.as_raw().iter().map(|x| lut[*x as usize]).collect();
@@ -44,11 +38,11 @@ pub fn scale_image(image: &GrayImage, mut peak_pixel_value: Option<u8>, gamma: f
 
 // In-place variant of scale_image().
 pub fn scale_image_mut(
-    image: &mut GrayImage, mut peak_pixel_value: Option<u8>, gamma: f32) {
-    if peak_pixel_value.is_none() {
-        peak_pixel_value = Some(get_peak_pixel(image));
+    image: &mut GrayImage, mut min_pixel_value: u8, peak_pixel_value: u8, gamma: f32) {
+    if min_pixel_value > peak_pixel_value / 2 {
+        min_pixel_value = peak_pixel_value / 2;
     }
-    let lut = compute_lut(peak_pixel_value, gamma);
+    let lut = compute_lut(min_pixel_value, peak_pixel_value, gamma);
 
     for pixel in image.pixels_mut() {
         pixel[0] = lut[pixel[0] as usize];
