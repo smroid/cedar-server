48345b6e2de875156d9978e4d686528cf0d5143a
smroid <smr@dt3.org>
2024-07-25 22:40:42 -0700

    updated cedar_sky.proto and cedar_sky_trait to match

--- src/cedar_sky_trait.rs
+++ src/cedar_sky_trait.rs
@@ -1,10 +1,10 @@
 // Copyright (c) 2024 Steven Rosenthal smr@dt3.org
 // See LICENSE file in root directory for license terms.
 
-use crate::cedar_sky::{CatalogConstraint, CatalogDescription,
+use crate::cedar_sky::{CatalogMatch, CatalogDescription,
                        Constellation, LocationInfo, ObjectType,
-                       ObjectTypeConstraint, Ordering,
-                       SkyLocationConstraint, SelectedCatalogEntry};
+                       ObjectTypeMatch, Ordering, SelectedCatalogEntry};
+use crate::tetra3_server::CelestialCoord;
 use canonical_error::CanonicalError;
 
 pub trait CedarSkyTrait {
@@ -12,14 +12,15 @@ pub trait CedarSkyTrait {
     fn get_object_types(&self) -> Vec<ObjectType>;
     fn get_constellations(&self) -> Vec<Constellation>;
     fn query_catalog_entries(&self,
-                             sky_location_constraint: Option<&SkyLocationConstraint>,
+                             max_distance: Option<f32>,
                              min_elevation: Option<f32>,
                              faintest_magnitude: Option<f32>,
-                             catalog_constraint: Option<&CatalogConstraint>,
-                             object_type_constraint: Option<&ObjectTypeConstraint>,
+                             catalog_match: Option<&CatalogMatch>,
+                             object_type_match: Option<&ObjectTypeMatch>,
                              ordering: Option<Ordering>,
                              dedup_distance: Option<f32>,
                              decrowd_distance: Option<f32>,
+                             sky_location: Option<CelestialCoord>,
                              location_info: Option<&LocationInfo>)
                              -> Result<Vec<SelectedCatalogEntry>, CanonicalError>;
 }
